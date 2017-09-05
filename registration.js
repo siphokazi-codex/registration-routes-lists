0637c1ad55f4bf9c743e2653b2edefd586bcd7b1
siphokazi-codex <aplenis@webmail.co.za>
2017-09-05 11:38:04 +0200

    Modified registration Webapp routes using lists, added models

--- registration.js
+++ registration.js
@@ -2,38 +2,32 @@ module.exports = function(){
 
   const regList = [];
 
-  // const getForm = function(req, res){
-  //
-  //   var registration = req.params.registration;
-  //   res.render('registration/add', {registration : regList});
-  // };
-
   const add = function(req, res){
 
     var registration = req.body.registration;
-    //var regResult = registration.toUpperCase();
+    //var regResult = registration.substr(0,3).toUpperCase();
 
-    var foundRegistration = regList.find(function(currentRegistration){
-      return currentRegistration === registration;
-    });
+     var foundRegistration = regList.find(function(currentRegistration){
+       return currentRegistration === registration;
+     });
 
     if (!registration){
       req.flash('error', 'Registration Number should not be blank');
     }
-    else if (!foundRegistration){
-      regList.push(registration);
-      req.flash('addRegistration', 'Registration Number added successfully');
-    }
-    else{
-      req.flash('error', 'You have inputed registration number before, please input different registration number')
-    }
 
-    res.render('registration/add', {registration : regList});
-  }
+     else if (!foundRegistration){
+       regList.push(registration);
+       req.flash('addRegistration', 'Registration Number added successfully');
+     }
+     else{
+       req.flash('error', 'You have inputed registration number before, please input different registration number')
+     }
+
+     res.render('registration/add', {registration : regList});
+   }
 
   return {
 
-    //getForm,
     add
   }
 
